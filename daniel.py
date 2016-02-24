ff2bee040b5958f74d58ced127f1cff0b324af81
zhaoj <daniel.zhao@dowjones.com>
2016-02-24 16:52:36 -0500

    update flask

--- daniel.py
+++ daniel.py
@@ -1,9 +1,23 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from sqlite3 import dbapi2 as sqlite3
+
 app = Flask(__name__)
+
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'daniel.db'),
+    DEBUG=True,
+    SECRET_KEY='',
+    USERNAME='admin',
+    PASSWORD='admin'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    # return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
