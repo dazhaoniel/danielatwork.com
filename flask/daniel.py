a8fa58c1da8d91ff0f91d33ad68fb310ea18152e
Daniel Zhao <danielantoiny@gmail.com>
2016-03-04 23:56:57 -0500

    add custom meta

--- flask/daniel.py
+++ flask/daniel.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 # app.config['MYSQL_DATABASE_PORT'] = 8889
-# app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'root'
 # app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'test'
 
@@ -21,16 +21,29 @@ app.config.from_envvar('DANIEL_SETTINGS', silent=True)
 
 @app.route('/')
 def index():
-    cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * FROM wp_posts")
-    entries = cursor.fetchone()
+    cursor = mysql.get_db().cursor()
+    # Get Projects
+    cursor.execute('''SELECT * FROM wp_posts 
+        WHERE post_type='project' 
+        AND post_status='publish' 
+        ORDER BY post_date DESC''')
+    entries = cursor.fetchall()
+    # Get About
+    cursor.execute('''SELECT * FROM wp_posts WHERE ID=1251''')
+    about = cursor.fetchone()
     current_year = datetime.now().year
-    return render_template('index.html', entries=entries, year=current_year)
+    
+    return render_template('index.html', entries=entries, about=about, year=current_year)
 
 
-@app.route('/wheres-my-car-privacy-policy')
+@app.route('/wheres-my-car-privacy-policy/')
 def wmc_privacy_policy():
-    return render_template('wmc-privacy-policy.html')
+    cursor = mysql.get_db().cursor()
+    cursor.execute('''SELECT * FROM wp_posts WHERE ID=1389''')
+    policy = cursor.fetchone()
+    current_year = datetime.now().year
+
+    return render_template('wmc-privacy-policy.html', policy=policy, year=current_year)
 
 
 if __name__ == "__main__":
--- flask/schema.sql
+++ flask/schema.sql
@@ -1,13 +1,13 @@
 DROP TABLE if exists `dp_entries`;
 CREATE TABLE `dp_entries` (
-  `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `name` varchar(39) NOT NULL,
+  `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, # in wordpress
+  `name` varchar(39) NOT NULL, # in wordpress
   `url` varchar(39) NOT NULL,
   `year` varchar(39) NOT NULL, 
   `image_url` varchar(39) NOT NULL,
-  `description` varchar(256) NOT NULL,
-  `created` timestamp NOT NULL, 
-  `last_updated` timestamp NOT NULL
+  `description` varchar(256) NOT NULL, # in wordpress
+  `created` timestamp NOT NULL, # in wordpress
+  `last_updated` timestamp NOT NULL # in wordpress
   );
 
 DROP TABLE if exists `dp_stack`;
