2a845d27c08dcb096ecf580aaa14156b5ad306e4
Daniel Zhao <danielantoiny@gmail.com>
2016-03-11 20:48:23 -0500

    update navigation

--- flask/daniel.py
+++ flask/daniel.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 from datetime import datetime
 
--- flask/static/assets/js/main.js
+++ flask/static/assets/js/main.js
@@ -116,6 +116,13 @@
 				return false;
 			});
 
+			$('.avatar a').click(function () {
+				$('body,html').animate({
+					scrollTop: 0
+				}, 800);
+				return false;
+			});
+
 	});
 
 })(jQuery);
