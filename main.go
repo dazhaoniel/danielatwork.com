84650faeed89e087f3d7258d8505b83d16a2d164
Daniel Zhao <daniel.zhao@weebly.com>
2019-07-31 13:50:33 -0400

    update text copy

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-  // "log"
   "net/http"
   "google.golang.org/appengine"
 )
@@ -10,7 +9,6 @@ func main() {
   fs := http.FileServer(http.Dir("flask"))
   http.Handle("/", fs)
 
-  // log.Println("Listening...")
   // http.ListenAndServe(":3000", nil)
   appengine.Main()
 }
